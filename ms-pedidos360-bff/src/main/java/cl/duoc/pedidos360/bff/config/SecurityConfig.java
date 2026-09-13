f02b7fe23972d342286a1ba915227894a1495a4c
Jorge Albornoz <jor.albornoz@duocuc.cl>
2026-09-13 12:33:34 -0300

    fix(bff): agregar origen de produccion a configuracion CORS

--- ms-pedidos360-bff/src/main/java/cl/duoc/pedidos360/bff/config/SecurityConfig.java
+++ ms-pedidos360-bff/src/main/java/cl/duoc/pedidos360/bff/config/SecurityConfig.java
@@ -84,7 +84,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList(
+            "http://localhost:4200",
+            "https://thankful-river-06284c40f.5.azurestaticapps.net"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
